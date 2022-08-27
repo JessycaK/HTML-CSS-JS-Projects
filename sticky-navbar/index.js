33dbac0eb4710fee97461adf031c97fa88275b6b
Jessyca Koufidissa <jessyca.m.koufidissa@gmail.com>
2022-08-27 12:37:15 -0500

    added project

--- sticky-navbar/index.js
+++ sticky-navbar/index.js
@@ -5,13 +5,13 @@ console.log(navbarEl.offsetHeight);
 
 console.log(bottomContainerEl.offsetTop);
 
-window.addEventListener("scroll",() => {
-    if (
-        window.scrollY >
-        bottomContainerEl.offsetTop -navbarEl.offsetHeight -50
-    ) {
-        navbarEl.classList.add("active");
-    } else {
-        navbarEl.classList.remove("active");
-    }
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY >
+    bottomContainerEl.offsetTop - navbarEl.offsetHeight - 50
+  ) {
+    navbarEl.classList.add("active");
+  } else {
+    navbarEl.classList.remove("active");
+  }
 });
